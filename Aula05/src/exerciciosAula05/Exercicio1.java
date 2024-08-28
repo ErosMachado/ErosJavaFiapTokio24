ab6e22096d428e1690014bc88e673418d454aefe
Eros Machado <112194183+ErosMachado@users.noreply.github.com>
2024-08-28 08:21:32 -0300

    Update Exercicio1.java
    mudanças

--- Aula05/src/exerciciosAula05/Exercicio1.java
+++ Aula05/src/exerciciosAula05/Exercicio1.java
@@ -5,8 +5,8 @@ public class Exercicio1 {
 	public static void main(String[] args) {
 		String nomeCompleto, endereco, cep, telefone;
 		nomeCompleto = "Eros Machado";
-		endereco = "Rua Giuseppe Marino, 200";
-		cep = "02181-190";
-		telefone = "(11) 98373-4326";
+		endereco = "Rua Teste, 123";
+		cep = "12345-123";
+		telefone = "(11) 98111-1111";
 	}
 }
